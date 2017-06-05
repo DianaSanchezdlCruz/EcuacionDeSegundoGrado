06c9aa2f005386ccde7144d14c75e94ec9f9710e
diana-sanchez-de-la-cruz <16470289@itcampeche.edu.mx>
2017-06-04 21:47:03 -0500

    Proyecto con la implementacion de la clase Main2

--- src/ecuaciondesegundogrado/Main.java
+++ src/ecuaciondesegundogrado/Main.java
@@ -3,7 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ecuaciondesegundogrado;
+package Ecuaciondesegundogrado;
+
+import java.util.Scanner;
+import modelo.EcuacionDeSegundoGrado;
 
 /**
  *
@@ -14,8 +17,58 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public static void main(String[] args) {
+       Scanner entrada=new  Scanner (System.in);
+       EcuacionDeSegundoGrado ecuacion = new EcuacionDeSegundoGrado(0,0,0);
+        
+       double a,b,c; 
+       boolean raiz;
+       System.out.println("Proporcionar el valor de a:");
+       a=entrada.nextDouble();
+       ecuacion.setCoeficienteA(a);
+       System.out.println("Proporcionar el valor de b:");
+       b=entrada.nextDouble();
+       ecuacion.setCoeficienteB(b);
+       System.out.println("Proporcionar el valor de c");
+       c=entrada.nextDouble();
+       ecuacion.setCoeficienteC(c);
+       
+       System.out.println("Gracias por Proporcionar los valores.");
+       System.out.println("El valor del Discriminante es: " + ecuacion.getDiscriminante());
+       System.out.print("Esta ecuacion ");
+       if (ecuacion.getNumeroDeSolucionesReales()==2)
+       {
+           System.out.println("tiene 2 Raices.");
+           raiz=true;
+       }
+       else
+       {
+           if(ecuacion.getNumeroDeSolucionesReales()==1)
+           {
+               System.out.println("tienes 1 Raiz.");
+               raiz=true;
+           }
+           else
+           {
+               System.out.println("no tiene Raiz.");
+               raiz=false;
+           }
+       }
+       if(raiz==true)
+       {
+           if(ecuacion.getNumeroDeSolucionesReales()==2)
+           {
+               System.out.println("Las raices de esta ecuacion son: x1=" + ecuacion.getRaiz1() + 
+                                    "   x2=" + ecuacion.getRaiz2());
+           }
+           else
+               if(ecuacion.getNumeroDeSolucionesReales()==1)
+               {
+                   System.out.println("La raiz de esta ecuacion es: x1=" + ecuacion.getRaiz1());
+               }
+       }
     }
     
 }
+
+
--- src/modelo/EcuacionDeSegundoGrado.java
+++ src/modelo/EcuacionDeSegundoGrado.java
@@ -10,5 +10,80 @@ package modelo;
  * @author Sanchez de la Cruz
  */
 public class EcuacionDeSegundoGrado {
+    private double a,b,c;
+    private double discriminante,resultado,numeroDeSoluciones,raiz1,raiz2;
+    
+    public EcuacionDeSegundoGrado (double a,double b, double c)
+    {
+        this.a=0;
+        this.b=0;
+        this.c=0;
+    }
+    
+    public double getCoeficienteA()
+    {
+        return a;
+    }
+    public double getCoeficienteB ()
+    {
+        return b;
+    }
+    public double getCoeficienteC()
+    {
+        return c;
+    }
+    
+    public void setCoeficienteA(double a)
+    {
+        this.a=a;
+    }
+    public void setCoeficienteB(double b)
+    {
+        this.b=b;
+    }
+    public void setCoeficienteC(double c)
+    {
+        this.c = c;
+    }
+    
+    public double getDiscriminante()
+    {
+        resultado= Math.pow(b,2);
+        discriminante=resultado - ((4*a)*(c));
+        return discriminante;
+    }
+    
+    public double getNumeroDeSolucionesReales()
+    {
+        if(discriminante>0)
+        {
+            numeroDeSoluciones=2;
+        }
+        else
+        {
+            if(discriminante==0)
+            {
+                numeroDeSoluciones=1;
+            }
+            else
+            {
+                numeroDeSoluciones=0;
+            }
+        }
+        return numeroDeSoluciones;
+    }
+    
+    public double getRaiz1()
+    {
+        raiz1=(-b + Math.sqrt(resultado- (4*(a)*(c))))/(2*a);
+        return raiz1;
+    }
+    public double getRaiz2()
+    {
+        raiz2=(-b - Math.sqrt(resultado- (4*(a)*(c))))/(2*a);
+        return raiz2;
+    }
     
 }
+
+
